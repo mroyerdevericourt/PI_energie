ff1b6e64580747502d9bf9134ea20c83048a27a2
mroyerdevericourt <mat.vericourt@gmail.com>
2021-03-09 19:33:03 +0100

    premier code et test

--- code/requete.py
+++ code/requete.py
@@ -1,1 +1,10 @@
 ## ce fichier sert à coder l'interface code/data (étape 2)
+
+import pandas as pd
+import numpy as np
+
+try :
+    fichier = pd.read_csv('data/DLE-elec_adresse_2018.csv', ";")
+    print(fichier.head())
+except FileNotFoundError as e:
+    print (e)
