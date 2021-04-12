30e0b08517c73290c732ae1a7783c4705d92f46d
mroyerdevericourt <mat.vericourt@gmail.com>
2021-04-12 21:24:08 +0200

    enfin le multithreading de qualité

--- code/init_CCPM.py
+++ code/init_CCPM.py
@@ -1,56 +1,113 @@
-## ce fichier sert à coder l'interface code/data (étape 2)
-
+import multiprocessing as mp
 import pandas as pd
 import numpy as np
 import requests
 import json
 import time
 
-# try :
-adresse = pd.read_csv('data/DLE-elec_adresse_2019.csv', ";")
-#     print(adresse.head())
-# except FileNotFoundError as e:
-#     print (e)
+def send(queue, base):
+    for i in base.index:
+        adresse = base['ADRESSE'][i]
+        code_iris = base['CODE_IRIS'][i]
 
-villes = "Barbey. Blennes. Cannes Écluse. Chevry-en-Sereine. Courcelles-en-Bassée. Diant. Esmans. Forges. La Brosse-Montceaux. La Grande-Paroisse. Laval-en-Brie. Marolles-sur-Seine. Misy-sur-Yonne. Montereau-fault-Yonne. Montmachoux. Noisy-Rudignon. Saint-Germain-Laval. Salins. Thoury-Ferrottes. Varennes-sur-Seine. Voulx"
-villes = villes.upper().split('. ')
+        recherche = '+'.join(adresse.split())
+        citycode = str(code_iris)[:5]
+        url = f'https://api-adresse.data.gouv.fr/search/?q={recherche}&citycode={citycode}&limit=1'
+        queue.put((i, url))
+    for _ in range(nb_proc):
+        queue.put((-1, None))
+
+def make_requests(queue_url, queue_json):
+    running = True
+    while running:
+        url_id, url = queue_url.get()
+        if url_id == -1 :
+            # print(f'ca va se finir pour {pid}')
+            running = False
+            break
+        status = 0
+        while status != 200:
+            reponse = requests.get(url)
+            status = reponse.status_code
+        datum = reponse.json()
+        queue_json.put((url_id, datum))
+
+def class_requests(base, queue_json, classed_json, long, sortie):
+    print("Requêtes à l'API Etalab en cours")
+    tampon = {}
+    cpt = 0
+    for i in base.index:
+        cpt += 1
+        print(f'{cpt*100/long: .2f} %')
+        try :
+            right_json = tampon[i]
+            del tampon[i]
+        except KeyError:
+            j, datum = queue_json.get()
+            while j != i:
+                tampon[j] = datum
+                j, datum = queue_json.get()
+            right_json = datum
+        classed_json.append(right_json)
+    print('Classement des requêtes terminé.')
+    treat_json(base, classed_json, sortie)
+
+def treat_json(base, L, sortie):
+    erreur = 0
+    Lon = []
+    Lat = []
+    Score = []
+    for elem in L:
+        try : 
+            lon, lat = elem["features"][0]["geometry"]["coordinates"]
+            score = elem["features"][0]["properties"]["score"]
+        except IndexError:
+            erreur += 1
+        Lon.append(lon)
+        Lat.append(lat)
+        Score.append(score)
+    print(f'Traitement terminé, il y a eu {erreur} erreur(s)')
+    print('Encryptage dans', sortie)
+    base = base.assign(latitude = Lat, longitude = Lon, score = Score)
+    print(base.head())
+    base.to_csv(sortie, sep = ';')
+    print('\n Encryptage terminé')
+    
 
 
-base = adresse[(adresse["NOM_COMMUNE"].isin(villes)) & pd.Series(map(lambda x : x[:2] == '77', adresse["CODE_IRIS"]))]
-print (base.head())
-longueur = base.shape[0]
-# new_df["latitude"] = [None]*longueur
-# new_df["longitude"] = [None]*longueur
-# try: 
-    # effacer la base de donnée créée
 
-base.to_csv('data/DLE-elec-2019-CCPM.csv',sep = ';')
-erreur = 0
-i=0
-def get_location(adresse, code_iris, ville=''):
-    global i
-    i += 1
-    print(f'{i*100/longueur : .3f} %')
-    recherche = '+'.join((adresse + ville).split())
-    citycode = str(code_iris)[:5]
-    url = f'https://api-adresse.data.gouv.fr/search/?q={recherche}&citycode={citycode}&limit=1'
-    status = 0
-    while status != 200:
-        reponse = requests.get(url)
-        status = reponse.status_code
-    datum = reponse.json()
-    try : 
-        lon, lat = datum["features"][0]["geometry"]["coordinates"]
-    except IndexError:
-        erreur += 1
-        return None, None
-    return lat, lon
+nb_proc = 10
 
+if __name__ == '__main__':
+    
+    for data_type in {'elec', 'gaz'}:
+        fichier = f'data/DLE-{data_type}_adresse_2019.csv'
+        fichier_sortie = f'data/DLE-{data_type}-2019-CCPM.csv'
+        fichier_sortie_geocoded = fichier_sortie[:-4] + '-geocoded.csv'
+        adresse = pd.read_csv(fichier, ";")
 
-Res = list(map(get_location, base["ADRESSE"], base["CODE_IRIS"]))
-Latit = [elem[0] for elem in Res]
-Long = [elem[1] for elem in Res]
-base = base.assign(latitude = Latit, longitude = Long)
+        villes = "Barbey. Blennes. Cannes Écluse. Chevry-en-Sereine. Courcelles-en-Bassée. Diant. Esmans. Forges. La Brosse-Montceaux. La Grande-Paroisse. Laval-en-Brie. Marolles-sur-Seine. Misy-sur-Yonne. Montereau-fault-Yonne. Montmachoux. Noisy-Rudignon. Saint-Germain-Laval. Salins. Thoury-Ferrottes. Varennes-sur-Seine. Voulx"
+        villes = villes.upper().split('. ')
 
-print(base.head())
-base.to_csv('data/DLE-elec-2019-CCPM-geocoded.csv',sep = ';')
+        base = adresse[(adresse["NOM_COMMUNE"].isin(villes)) & pd.Series(map(lambda x : x[:2] == '77', adresse["CODE_IRIS"]))]
+
+        base.to_csv(fichier_sortie, sep = ';')
+
+        long_base = base.shape[0]
+        print('Geocodage de', fichier)
+        
+        url_queue = mp.Queue()
+        json_queue = mp.Queue()
+        classed_json = []
+        url_process = mp.Process(target = send, args = (url_queue, base,))
+        url_process.start()
+        requests_processes = [mp.Process(target = make_requests, args = (url_queue, json_queue)) for pid in range(nb_proc)]
+        for proc in requests_processes:
+            proc.start()
+        classing_proc = mp.Process(target = class_requests, args = (base, json_queue, classed_json, long_base, fichier_sortie_geocoded))
+        classing_proc.start()
+        
+        classing_proc.join()
+        url_process.join()
+        for proc in requests_processes:
+            proc.join()
