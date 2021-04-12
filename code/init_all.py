d21a028646a5b11231ae2921648fc907a32f15e7
mroyerdevericourt <mat.vericourt@gmail.com>
2021-04-12 22:34:16 +0200

    multiprocess pour init_all

--- code/init_all.py
+++ code/init_all.py
@@ -1,61 +1,107 @@
-## ce fichier sert à géocoder la table entière de données.
-
+import multiprocessing as mp
 import pandas as pd
 import numpy as np
 import requests
 import json
 import time
 
+def send(queue, base):
+    for i in base.index:
+        adresse = base['ADRESSE'][i]
+        code_iris = base['CODE_IRIS'][i]
+        citycode = str(code_iris)[:5]
+        if type(adresse) != str:
+            url = f'https://api-adresse.data.gouv.fr/search/?q=citycode={citycode}&limit=1'
+        else :
+            recherche = '+'.join(adresse.split())
+            url = f'https://api-adresse.data.gouv.fr/search/?q={recherche}&citycode={citycode}&limit=1'
+        queue.put((i, url))
+    for _ in range(nb_proc):
+        queue.put((-1, None))
 
-try : 
-    adresse = pd.read_csv('data/DLE-elec-2019-geocoded.csv', ";")
-except FileNotFoundError :
-    adresse = pd.read_csv('data/DLE-elec_adresse_2019.csv', ";")
-base = adresse
-print(base.head())
-longueur = base.shape[0]
-N = 1000
-i = begin(base)
+def make_requests(queue_url, queue_json):
+    running = True
+    while running:
+        url_id, url = queue_url.get()
+        if url_id == -1 :
+            # print(f'ca va se finir pour {pid}')
+            running = False
+            break
+        status = 0
+        while status != 200:
+            reponse = requests.get(url)
+            status = reponse.status_code
+        datum = reponse.json()
+        queue_json.put((url_id, datum))
 
-while i < longueur :
-    j = 0
-    Res = [None]*longueur
-    while j < N and i < longueur:
-        j += 1
-        i += 1
-        print(f'Global = {i*100/longueur:.4f} %, Cycle = {j*100/N:.2f} %')
+def class_requests(base, queue_json, classed_json, long, sortie):
+    print("Requêtes à l'API Etalab en cours")
+    tampon = {}
+    cpt = 0
+    for i in base.index:
+        cpt += 1
+        print(f'{cpt*100/long: .3f} %')
+        try :
+            right_json = tampon[i]
+            del tampon[i]
+        except KeyError:
+            j, datum = queue_json.get()
+            while j != i:
+                tampon[j] = datum
+                j, datum = queue_json.get()
+            right_json = datum
+        classed_json.append(right_json)
+    print('Classement des requêtes terminé.')
+    treat_json(base, classed_json, sortie)
+
+def treat_json(base, L, sortie):
+    erreur = 0
+    Lon = []
+    Lat = []
+    Score = []
+    for elem in L:
+        try : 
+            lon, lat = elem["features"][0]["geometry"]["coordinates"]
+            score = elem["features"][0]["properties"]["score"]
+        except IndexError:
+            erreur += 1
+        Lon.append(lon)
+        Lat.append(lat)
+        Score.append(score)
+    print(f'Traitement terminé, il y a eu {erreur} erreur(s)')
+    print('Encryptage dans', sortie)
+    base = base.assign(latitude = Lat, longitude = Lon, score = Score)
+    print(base.head())
+    base.to_csv(sortie, sep = ';')
+    print('\n Encryptage terminé')
     
-    Latit = [elem[0] for elem in Res]
-    Long = [elem[1] for elem in Res]
-    base = base.assign(latitude = Latit)
-    base = base.assign(longitude = Long)
+
+
+
+nb_proc = 10
+
+if __name__ == '__main__':
     
-    base.to_csv('data/DLE-elec-2019-geocoded.csv',sep = ';')
+    for data_type in {'elec', 'gaz'}:
+        fichier = f'data/DLE-{data_type}_adresse_2019.csv'
+        fichier_sortie_geocoded = fichier[:-4] + '-geocoded.csv'
+        base = pd.read_csv(fichier, ";")
 
-
-
-def get_location(adresse, code_iris, ville=''):
-    recherche = '+'.join((adresse + ville).split())
-    citycode = str(code_iris)[:5]
-    url = f'https://api-adresse.data.gouv.fr/search/?q={recherche}&citycode={citycode}&limit=1'
-    status = 0
-    while status != 200:
-        reponse = requests.get(url)
-        status = reponse.status_code
-    datum = reponse.json()
-    try : 
-        lon, lat = datum["features"][0]["geometry"]["coordinates"]
-    except IndexError:
-        return None, None
-    properties = datum["features"][0]["properties"]
-    return lat, lon, properties
-
-
-# Res = list(map(get_location, base["ADRESSE"], base["CODE_IRIS"]))
-# Latit = [elem[0] for elem in Res]
-# Long = [elem[1] for elem in Res]
-# base = base.assign(latitude = Latit)
-# base = base.assign(longitude = Long)
-
-# print(base.head())
-# base.to_csv('data/DLE-elec-2019-geocoded.csv',sep = ';')
+        long_base = base.shape[0]
+        print('Geocodage de', fichier)
+        
+        url_queue = mp.Queue()
+        json_queue = mp.Queue()
+        classed_json = []
+        url_process = mp.Process(target = send, args = (url_queue, base,))
+        url_process.start()
+        requests_processes = [mp.Process(target = make_requests, args = (url_queue, json_queue)) for pid in range(nb_proc)]
+        for proc in requests_processes:
+            proc.start()
+        classing_proc = mp.Process(target = class_requests, args = (base, json_queue, classed_json, long_base, fichier_sortie_geocoded))
+        classing_proc.start()
+        
+        classing_proc.join()
+        url_process.join()
+        for proc in requests_processes:
+            proc.join()
