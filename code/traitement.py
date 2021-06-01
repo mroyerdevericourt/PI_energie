e2092bca029899202e819d305ae7f6f3293ea666
mroyerdevericourt <mat.vericourt@gmail.com>
2021-06-01 15:27:12 +0200

    correction code_secteur et autres
    affiche désormais le premier non nul dans les colonnes concernées lors de l'agrégation par id

--- code/traitement.py
+++ code/traitement.py
@@ -41,34 +41,48 @@ gaz['CONSO'] = gaz['CONSO'].apply(convert_e2f)
 #     print(base_useful)
 
 # elec, gaz = bases_useful
+
+### On met en forme gaz et elec, et on les fusionne sur leurs adresses
 elec.rename(columns = {'CONSO': 'CONSO_elec', 'PDL' : 'PDL_elec', 'id': 'id_geo'}, inplace = True)
 gaz.rename(columns = {'CONSO': 'CONSO_gaz', 'PDL' : 'PDL_gaz', 'id': 'id_geo'}, inplace = True)
 keys = ('FILIERE', 'CODE_GRAND_SECTEUR', 'ADRESSE', 'NOM_COMMUNE', 'latitude', 'longitude', 'id_geo', 'score')
 joined = elec.merge(gaz, on = keys, how = 'outer')
 
+### On introduit la consommation brute du logement, qui nous servira par la suite pour faire des groupements
 for elem in {'consommation_energie', 'estimation_ges'}:
     dpe[elem + '_brut'] = (dpe[elem] * dpe['surface_habitable'])
 
+
+### On traite les DPE
 # print(dpe[['consommation_energie', 'consommation_energie_brut', 'surface_habitable', 'estimation_ges', 'estimation_ges_brut']])
 dpe.rename(columns = {elem : elem + '_dpe' for elem in ['commune', 'type_voie', 'numero_rue', 'nom_rue', 'id']} , inplace = True)
 dpe.rename(columns = {'result_id': 'id_geo', 'result_score' : 'score'} , inplace = True)
 
+### On joint tout
 all_join = dpe.merge(joined, on = ('latitude', 'longitude', 'score', 'id_geo'), how = 'outer')
+all_join.to_csv('data/test.csv', ';')
 
+
+### On va distinguer les colonnes intéressantes en fonction de l'opération d'agrégation, et on les joint de nouveau
 grouped = all_join.groupby('id_geo')
 sumed = grouped[['consommation_energie_brut', 'estimation_ges_brut', 'surface_habitable', 'PDL_elec', 'CONSO_elec', 'PDL_gaz', 'CONSO_gaz']].sum()
 meaned = grouped[['latitude', 'longitude', 'score']].mean()
 firsted = grouped[['classe_consommation_energie', 'classe_estimation_ges', 'secteur_activite', 'commune_dpe', 'type_voie_dpe', 
-                   'numero_rue_dpe', 'nom_rue_dpe', 'result_label', 'result_type', 'result_housenumber', 'result_name', 'NOM_COMMUNE', 'ADRESSE', 'FILIERE', 'CODE_GRAND_SECTEUR']].nth(0)
+                   'numero_rue_dpe', 'nom_rue_dpe', 'result_label', 'result_type', 'result_housenumber', 'result_name', 'NOM_COMMUNE', 'ADRESSE', 'FILIERE', 'CODE_GRAND_SECTEUR']].first()
+
 
 merge1 = firsted.merge(meaned, on = 'id_geo')
 dpe_dle = merge1.merge(sumed, on = 'id_geo')
 
+
+### On rajoute les classes de consommation et d'émission
 for elem in {'consommation_energie', 'estimation_ges'}:
     dens = (dpe_dle[elem + '_brut'] / dpe_dle['surface_habitable'])
     classes = dens.apply(encadrement, args = (type_encadrement[elem],))
     dpe_dle['classe_' + elem] = classes
+
 dpe_dle.to_csv('data/dpe_dle-pertinent.csv', ';')
+
 
 
 
