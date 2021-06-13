db51277a4cb7e470b7f21a76ac0e2440faeae49e
mroyerdevericourt <mat.vericourt@gmail.com>
2021-06-13 18:06:10 +0200

    Commentaires
    Nouveaux commentaires sur les fonctions de init_all et traitement

--- code/traitement.py
+++ code/traitement.py
@@ -1,3 +1,7 @@
+### Ce programme utilise les bases de données DLE (elec et gaz) géocodées et DPE 
+### afin de ne garder que les données intéressantes à utiliser sur la carte.
+### Il rassemble aussi les différentes données autour de l'id d'Etalab (propre à chaque bâtiment).
+
 import pandas as pd
 import math
 
@@ -12,7 +16,6 @@ limite_classe_energie = {50 : 'A', 90 : 'B', 150 : 'C', 230 : 'D', 330 : 'E', 45
 limite_classe_ges = {5 : 'A', 10 : 'B', 20 : 'C', 35 : 'D', 55 : 'E', 80 : 'F', math.inf : 'G'}
 type_encadrement = {'consommation_energie' : limite_classe_energie, 'estimation_ges' : limite_classe_ges}
 
-
 def encadrement(val, inter):
     for key in inter:
         classe = inter[key]
@@ -20,27 +23,8 @@ def encadrement(val, inter):
             break
     return classe
 
-
 elec['CONSO'] = elec['CONSO'].apply(convert_e2f)
 gaz['CONSO'] = gaz['CONSO'].apply(convert_e2f)
-# Aggregation sur l'id d'etalab
-
-# toBfirsted = {elec : ['FILIERE', 'CODE_GRAND_SECTEUR']}
-# toBmeaned = {elec : }
-# toBsumed
-
-# bases_useful = []
-# for base in [elec, gaz]:
-#     groupe = base.groupby('id')
-#     firsted = groupe[['FILIERE', 'CODE_GRAND_SECTEUR', 'ADRESSE', 'NOM_COMMUNE']].nth(0)
-#     meaned = groupe[['latitude', 'longitude']].mean()
-#     sumed = groupe[['PDL', 'CONSO']].sum()
-#     grouped = firsted.merge(meaned, on = 'id')
-#     base_useful = grouped.merge(sumed, on = 'id')
-#     bases_useful.append(base_useful)
-#     print(base_useful)
-
-# elec, gaz = bases_useful
 
 ### On met en forme gaz et elec, et on les fusionne sur leurs adresses
 elec.rename(columns = {'CONSO': 'CONSO_elec', 'PDL' : 'PDL_elec', 'id': 'id_geo'}, inplace = True)
@@ -54,7 +38,6 @@ for elem in {'consommation_energie', 'estimation_ges'}:
 
 
 ### On traite les DPE
-# print(dpe[['consommation_energie', 'consommation_energie_brut', 'surface_habitable', 'estimation_ges', 'estimation_ges_brut']])
 dpe.rename(columns = {elem : elem + '_dpe' for elem in ['commune', 'type_voie', 'numero_rue', 'nom_rue', 'id']} , inplace = True)
 dpe.rename(columns = {'result_id': 'id_geo', 'result_score' : 'score'} , inplace = True)
 
@@ -65,12 +48,13 @@ all_join.to_csv('data/test.csv', ';')
 
 ### On va distinguer les colonnes intéressantes en fonction de l'opération d'agrégation, et on les joint de nouveau
 grouped = all_join.groupby('id_geo')
-sumed = grouped[['consommation_energie_brut', 'estimation_ges_brut', 'surface_habitable', 'PDL_elec', 'CONSO_elec', 'PDL_gaz', 'CONSO_gaz']].sum()
+sumed = grouped[['consommation_energie_brut', 'estimation_ges_brut', 'surface_habitable', 
+                    'PDL_elec', 'CONSO_elec', 'PDL_gaz', 'CONSO_gaz']].sum()
 meaned = grouped[['latitude', 'longitude', 'score']].mean()
-firsted = grouped[['classe_consommation_energie', 'classe_estimation_ges', 'secteur_activite', 'commune_dpe', 'type_voie_dpe', 
-                   'numero_rue_dpe', 'nom_rue_dpe', 'result_label', 'result_type', 'result_housenumber', 'result_name', 'NOM_COMMUNE', 'ADRESSE', 'FILIERE', 'CODE_GRAND_SECTEUR']].first()
-
-
+firsted = grouped[['classe_consommation_energie', 'classe_estimation_ges', 'secteur_activite', 
+                    'commune_dpe', 'type_voie_dpe','numero_rue_dpe', 'nom_rue_dpe', 'result_label', 
+                    'result_type', 'result_housenumber', 'result_name', 'NOM_COMMUNE', 'ADRESSE', 
+                    'FILIERE', 'CODE_GRAND_SECTEUR']].first()
 merge1 = firsted.merge(meaned, on = 'id_geo')
 dpe_dle = merge1.merge(sumed, on = 'id_geo')
 
@@ -82,28 +66,3 @@ for elem in {'consommation_energie', 'estimation_ges'}:
     dpe_dle['classe_' + elem] = classes
 
 dpe_dle.to_csv('data/dpe_dle-pertinent.csv', ';')
-
-
-
-
-
-# def convert_e2f(string):
-#     return float(string[0] + string[2:8]) * 10**int(string[9:])
-# print(gaz.dtypes)
-# for base in [elec, gaz]:
-#     for key in ["CONSO", "PDL", "latitude", "longitude", "score"]:
-#         print(base[key])
-#         # base[key].transform(convert_e2f)
-
-# for key in ["consommation_energie", "estimation_ges", "surface_habitable", "latitude", "longitude", "result_score"]:
-#     # dpe[key].transform(convert_e2f)
-#     pass
-
-
-# joined = gaz.merge(elec, 'outer', "id")
-# print(joined)
-# dpe_grouped = dpe.groupby("result_id")
-# print(dpe_grouped)
-
-# joined = joined.merge(dpe, 'outer', left_on = "id", right_on = "result_id")
-# print(joined)
